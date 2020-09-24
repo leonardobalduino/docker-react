f6a4d0391d5be3091909227b7341cbd16e9448a4
leonardobalduino <leonardobalduino@hotmail.com>
2020-09-24 06:44:10 -0300

    modified files

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import ReactDOM from 'react-dom';
 import App from './App';
 
-test('renders without crashing', () => {
+it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
